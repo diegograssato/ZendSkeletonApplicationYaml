432210cfbcca547bbcd5903dfc91eced8f246b08
Jesus Vieira <jesusvieiradelima@gmail.com>
2014-03-12 20:17:01 -0400

    Preparando para o pull

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -13,13 +13,28 @@ class IndexController extends AbstractActionController
 {
     /**
      * @Route\Literal("/", name="home")
+     * @Route\Segment("/[page/:page]", constraints={"page"="\d+"}, name="paginacao")
      */
     public function indexAction()
     {
         return new ViewModel();
     }
 
+    /**
+     * @Route\Segment("/editar[/:id]", constraints={"id"="\d+"})
+     * @return ViewModel
+     */
     public function novoAction()
+    {
+        return new ViewModel(array('data' => 'Olá este é o editar!'));
+    }
+
+
+    /**
+     * @Route\Segment("/novo[/:id]", constraints={"id"="\d+"})
+     * @return ViewModel
+     */
+    public function editarAction()
     {
         return new ViewModel(array('data' => 'Olá este é novo!'));
     }
